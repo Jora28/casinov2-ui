b78284fbbd0a671bad30cd4bb507d61a085828cb
Arus Sayamyan <arus.sayamyan@betfounders.com>
2025-07-22 11:10:20 +0400

    change import url

--- playground/app.vue
+++ playground/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class=" flex flex-col items-center justify-center">
+  <div class="flex flex-col items-center justify-center">
     Nuxt module playground!
     <div>
       <CasinoButton>
