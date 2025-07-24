8779905593979a1c8d5240dcec32b5292cadfd15
Jora Mkrtchyan <j.mkrtchyan@innoverse.tech>
2025-07-24 11:54:42 +0400

    add headlessui

--- playground/components/Sidebar.vue
+++ playground/components/Sidebar.vue
@@ -1,37 +1,51 @@
 <template>
-  <div class="h-full">
-    <UAccordion :items="items">
-      <template #content>
-        <p
-          v-for="component in allComponents"
-          class="px-8 py-4 hover:bg-[#4f5a82] cursor-pointer"
-        >
-          {{ component }}
-        </p>
-      </template>
-    </UAccordion>
+  <div class="w-full px-4 pt-16">
+    <div class="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
+      <Disclosure v-slot="{ open }">
+        <DisclosureButton
+          class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+          <span>What is your refund policy?</span>
+          <!-- <ChevronUpIcon :class="open ? 'rotate-180 transform' : ''" class="h-5 w-5 text-purple-500" /> -->
+        </DisclosureButton>
+        <DisclosurePanel class="px-4 pb-2 pt-4 text-sm text-gray-500">
+          If you're unhappy with your purchase for any reason, email us within
+          90 days and we'll refund you in full, no questions asked.
+        </DisclosurePanel>
+      </Disclosure>
+      <Disclosure as="div" class="mt-2" v-slot="{ open }">
+        <DisclosureButton
+          class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+          <span>Do you offer technical support?</span>
+          <!-- <ChevronUpIcon :class="open ? 'rotate-180 transform' : ''" class="h-5 w-5 text-purple-500" /> -->
+        </DisclosureButton>
+        <DisclosurePanel class="px-4 pb-2 pt-4 text-sm text-gray-500">
+          No.
+        </DisclosurePanel>
+      </Disclosure>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { AccordionItem } from '@nuxt/ui'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+// import { ChevronUpIcon } from '@heroicons/vue/20/solid'
 
-const allComponents = ref([])
-const components = import.meta.glob('../../src/runtime/components/**/*.vue')
+// const allComponents = ref([])
+// const components = import.meta.glob('../../src/runtime/components/**/*.vue')
 
-for (const path in components) {
-  if (path) {
-    const name = path.split('/').pop()
-      .replace(/\.\w+$/, '')
+// for (const path in components) {
+//   if (path) {
+//     const name = path.split('/').pop()
+//       .replace(/\.\w+$/, '')
 
-    allComponents.value.push(name)
-  }
-}
+//     allComponents.value.push(name)
+//   }
+// }
 
-const items = ref<AccordionItem[]>([
-  {
-    label: 'Components',
-    icon: 'i-lucide-box',
-  },
-])
+// const items = ref<AccordionItem[]>([
+//   {
+//     label: 'Components',
+//     icon: 'i-lucide-box',
+//   },
+// ])
 </script>
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,12 +1,10 @@
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/ui',
     '../src/module',
   ],
   imports: {
     dirs: ['stores'],
   },
-  css: ['@/assets/style.css'],
   devtools: { enabled: true },
 
 })
